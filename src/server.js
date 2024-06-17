3568ade88d9755baf8e9b4ae758c40d7d6e28356
vannt97 <79388573+vannt97@users.noreply.github.com>
2024-06-17 08:50:51 +0700

    update

--- src/server.js
+++ src/server.js
@@ -7,53 +7,96 @@ const routerWeb = require("./routes/web");
 // const path = require("path");
 // app.use('/images', express.static(path.join(__dirname, '/public/images')));
 // config
-app.use(express.json());
-viewEngineConfig(app);
+// app.use(express.json());
+// viewEngineConfig(app);
 // Định nghĩa một route đơn giản
-app.use("/", routerWeb);
+// app.use("/", routerWeb);
 
 // Định nghĩa một API endpoint
-app.post("/api/save", (req, res) => {
-  const newItem = req.body;
-  const query = "INSERT INTO images (link) VALUES (?)";
-  connection.query(query, [newItem.link], (err, result) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    newItem.id = result.insertId;
-    res.json(newItem);
-  });
-});
+// app.post("/api/save", (req, res) => {
+//   const newItem = req.body;
+//   const query = "INSERT INTO images (link) VALUES (?)";
+//   connection.query(query, [newItem.link], (err, result) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     newItem.id = result.insertId;
+//     res.json(newItem);
+//   });
+// });
 
-app.get("/api/images/:id", (req, res) => {
-  const itemId = req.params.id;
-  const query = "SELECT * FROM images WHERE id = ?";
-  connection.query(query, [itemId], (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    if (results.length === 0) {
-      res.status(404).json({ error: "Item not found" });
-      return;
-    }
-    const json = {
-      messages: [
-        {
-          attachment: {
-            type: "image",
-            payload: {
-              url: results[0]?.link,
-            },
-          },
-        },
-      ],
-    };
-    res.json(json);
+// app.get("/api/images/:id", (req, res) => {
+//   const itemId = req.params.id;
+//   const query = "SELECT * FROM images WHERE id = ?";
+//   console.log("connection: ", connection);
+//   connection.query(query, [itemId], (err, results) => {
+//     console.log("err: ", err);
+//     console.log("results: ", results);
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     if (results.length === 0) {
+//       res.status(404).json({ error: "Item not found" });
+//       return;
+//     }
+//     const json = {
+//       messages: [
+//         {
+//           attachment: {
+//             type: "image",
+//             payload: {
+//               url: results[0]?.link,
+//             },
+//           },
+//         },
+//       ],
+//     };
+//     res.json(json);
+//   });
+// });
+connection
+  .init()
+  .then(() => {
+    console.log("ket noi db thanh cong");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server đang chạy tại http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("errrrr: ", err);
+    process.exit(1);
   });
+app.get("/api/images", async (req, res) => {
+  const items = await connection.getItems();
+  console.log("items: ", items);
+  res.send(items);
+
+  // const itemId = req.params.id;
+  // const query = "SELECT * FROM images WHERE id = ?";
+  // connection.query(query, [itemId], (err, results) => {
+  //   if (err) {
+  //     res.status(500).json({ error: err.message });
+  //     return;
+  //   }
+  //   if (results.length === 0) {
+  //     res.status(404).json({ error: "Item not found" });
+  //     return;
+  //   }
+  //   const json = {
+  //     messages: [
+  //       {
+  //         attachment: {
+  //           type: "image",
+  //           payload: {
+  //             url: results[0]?.link,
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   };
+  //   res.json(json);
+  // });
 });
 // Bắt đầu server
-app.listen(process.env.PORT, () => {
-  console.log(`Server đang chạy tại http://localhost:${process.env.PORT}`);
-});
